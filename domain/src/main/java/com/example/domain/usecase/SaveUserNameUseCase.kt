3cd115d6660e3aa4d5384ed3af7809e6fad292a8
Ilya <felagunt@live.ru>
2022-09-07 19:53:11 +0300

    SaveUserNameUseCase unit test

--- domain/src/main/java/com/example/domain/usecase/SaveUserNameUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/SaveUserNameUseCase.kt
@@ -6,6 +6,11 @@ import com.example.domain.repository.UserRepository
 class SaveUserNameUseCase(private val userRepository: UserRepository) {
 
     fun execute(param: SaveParam): Boolean {
+
+        val oldUserName = userRepository.getName()
+        if (oldUserName.firstName == param.name) {
+            return true
+        }
         return userRepository.saveName(saveParam = param)
     }
 }
